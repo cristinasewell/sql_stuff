6a14101e0c677bf75fd739e6c0087db0aca60210
cristinasewell <cristina.m.sewell@gmail.com>
2021-08-31 15:40:36 -0700

    MNT: sqlalchemy stuff

---
+++ sql_alchemy/challenge/app.py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# 1. Import Flask\n",
+    "from flask import Flask\n",
+    "\n",
+    "\n",
+    "# 2. Create an app\n",
+    "app = Flask(__name__)\n",
+    "\n",
+    "\n",
+    "# 3. Define static routes\n",
+    "@app.route(\"/\")\n",
+    "def index():\n",
+    "    return \"Hello, world!\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
